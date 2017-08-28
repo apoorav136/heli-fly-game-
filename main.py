94739bba3be99a53fed9a0fb6958992835cc59d8
Apoorav sharma <apooravsharma136@gmail.com>
2017-08-28 22:58:34 +0530

    obstacles has beens created for our helicopter..

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # py game is a library available for developing games in python
 import pygame, time
-
+from random import  *
 black = (0, 0, 0)
 white = (255, 255, 255)
 
@@ -8,6 +8,10 @@ pygame.init()
 # here we are creating a surface for our game given some heights and width to it
 surfaceWidth = 1000
 surfaceHeight = 500
+imageHeight = 43
+imageWidth = 100
+
+
 surface = pygame.display.set_mode((surfaceWidth, surfaceHeight))
 # caption is being provided to our game using display.set
 pygame.display.set_caption('heli-fly')
@@ -16,6 +20,10 @@ clock = pygame.time.Clock()
 
 img = pygame.image.load('heli.jpg')
 
+
+def blocks(x_block,y_block,block_width,block_height,gap):
+    pygame.draw.rect(surface,white,[x_block,y_block,block_width,block_height])
+    pygame.draw.rect(surface, white, [x_block, y_block+block_height+gap, block_width, surfaceHeight])
 
 def replay_or_quit():
     for event in pygame.event.get([pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT]):
@@ -67,8 +75,14 @@ def main():
     y = 200
     y_move = 0
 
-    game_over = False
+    x_block = surfaceWidth
+    y_block = 0
+    block_width = 75
+    block_height = randint(0, (surfaceHeight/2))
+    gap = imageHeight * 4.1
+    block_move = 3
 
+    game_over = False
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -86,12 +100,21 @@ def main():
 
         surface.fill(black)
         helicopter(x, y, img)
+        blocks(x_block, y_block, block_width, block_height,gap)
+        x_block -= block_move
 
         if y > surfaceHeight - 40 or y < 0:
             game_Over()
 
+        if x_block <(-1*block_width):
+            x_block = surfaceWidth
+            block_height = randint(0,surfaceHeight/2)
+
+
+
+
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(80)
 
 main()
 pygame.quit()
